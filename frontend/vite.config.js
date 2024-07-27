5852bc7b2f669431e6c63680f5815e60ff78dc77
Nutan Panta <nutanrajpanta@gmail.com>
2024-07-27 23:48:09 +0545

    adding deployed url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
+  server: {
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',}
-    }
-  }
-})
+        target: 'https://lets-chat-gpsy.onrender.com',
+      },
+    },
+  },
+});
