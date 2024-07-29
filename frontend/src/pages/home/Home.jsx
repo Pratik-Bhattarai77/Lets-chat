41728909948c5f33a73a52d3c7318dbddbcb5d50
Pratik Bhattarai <149646839+Pratik-Bhattarai77@users.noreply.github.com>
2024-07-29 23:14:36 +0545

    Update Home.jsx

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,12 +4,8 @@ import Sidebar from "../../components/sidebar/Sidebar";
 const Home = () => {
 	return (
 		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-			<div className='flex flex-col w-full sm:w-auto sm:flex-shrink-0'>
-				<Sidebar />
-			</div>
-			<div className='hidden sm:flex flex-col flex-1'>
-				<MessageContainer />
-			</div>
+			<Sidebar />
+			<MessageContainer />
 		</div>
 	);
 };
