307a1994133cba82cbaa7d17deacf9bae89b77d4
Pratik Bhattarai <149646839+Pratik-Bhattarai77@users.noreply.github.com>
2024-07-29 22:27:01 +0545

    Update MessageContainer.jsx

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -5,7 +5,7 @@ import Messages from "./Messages";
 import { TiMessages } from "react-icons/ti";
 import { useAuthContext } from "../../context/AuthContext";
 
-const MessageContainer = ({ setShowSidebar }) => {
+const MessageContainer = () => {
 	const { selectedConversation, setSelectedConversation } = useConversation();
 
 	useEffect(() => {
@@ -14,13 +14,13 @@ const MessageContainer = ({ setShowSidebar }) => {
 	}, [setSelectedConversation]);
 
 	return (
-		<div className='md:min-w-[450px] flex flex-col h-full'>
+		<div className='md:min-w-[450px] flex flex-col'>
 			{!selectedConversation ? (
 				<NoChatSelected />
 			) : (
 				<>
 					{/* Header */}
-					<div className='hidden sm:block bg-slate-500 px-4 py-2 mb-2'>
+					<div className='bg-slate-500 px-4 py-2 mb-2'>
 						<span className='label-text'>To:</span>{" "}
 						<span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
 					</div>
