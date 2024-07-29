9cae3ca5055d51c03cf4df595a85b0b870dcb419
Pratik Bhattarai <149646839+Pratik-Bhattarai77@users.noreply.github.com>
2024-07-29 22:31:00 +0545

    Update Home.jsx

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,33 +1,16 @@
-import { useState } from "react";
 import MessageContainer from "../../components/messages/MessageContainer";
 import Sidebar from "../../components/sidebar/Sidebar";
-import useConversation from "../../zustand/useConversation";
 
 const Home = () => {
-	const { selectedConversation } = useConversation();
-	const [showSidebar, setShowSidebar] = useState(true);
-
 	return (
 		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-			<div className={`${showSidebar ? 'flex' : 'hidden'} sm:flex flex-col w-full sm:w-auto`}>
+			<div className='flex flex-col w-full sm:w-auto sm:flex-shrink-0'>
 				<Sidebar />
 			</div>
-			<div className={`flex-1 ${showSidebar ? 'hidden' : 'flex'} sm:flex flex-col`}>
-				{selectedConversation && (
-					<div className="bg-slate-500 px-4 py-2 mb-2 flex justify-between items-center sm:hidden">
-						<button 
-							onClick={() => setShowSidebar(true)} 
-							className="text-white"
-						>
-							Back
-						</button>
-						<span className="text-gray-900 font-bold">{selectedConversation.fullName}</span>
-					</div>
-				)}
-				<MessageContainer setShowSidebar={setShowSidebar} />
+			<div className='hidden sm:flex flex-col flex-1'>
+				<MessageContainer />
 			</div>
 		</div>
 	);
 };
-
 export default Home;
