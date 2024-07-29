288060a2ad07df749f78f07ef249759735001b4b
Pratik Bhattarai <149646839+Pratik-Bhattarai77@users.noreply.github.com>
2024-07-29 22:51:09 +0545

    Update MessageContainer.jsx

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -9,17 +9,15 @@ const MessageContainer = () => {
 	const { selectedConversation, setSelectedConversation } = useConversation();
 
 	useEffect(() => {
-		// cleanup function (unmounts)
 		return () => setSelectedConversation(null);
 	}, [setSelectedConversation]);
 
 	return (
-		<div className='sm:min-w-[450px] flex flex-col h-full'>
+		<div className='flex flex-col h-full'>
 			{!selectedConversation ? (
 				<NoChatSelected />
 			) : (
 				<>
-					{/* Header */}
 					<div className='bg-slate-500 px-4 py-2 mb-2'>
 						<span className='label-text'>To:</span>{" "}
 						<span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
@@ -31,7 +29,6 @@ const MessageContainer = () => {
 		</div>
 	);
 };
-export default MessageContainer;
 
 const NoChatSelected = () => {
 	const { authUser } = useAuthContext();
@@ -45,3 +42,5 @@ const NoChatSelected = () => {
 		</div>
 	);
 };
+
+export default MessageContainer;
