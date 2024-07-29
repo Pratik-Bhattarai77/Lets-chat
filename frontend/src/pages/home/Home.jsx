92be880e0735ebd20872c7dfb8d3f320da41d326
Pratik Bhattarai <149646839+Pratik-Bhattarai77@users.noreply.github.com>
2024-07-29 22:08:53 +0545

    Update Home.jsx

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,15 +2,15 @@ import MessageContainer from "../../components/messages/MessageContainer";
 import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
-    return (
-        <div className='flex flex-col sm:flex-row w-full max-w-7xl mx-auto h-[90vh] sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-            <div className="w-full sm:w-1/3 md:w-1/4">
-                <Sidebar />
-            </div>
-            <div className="w-full sm:w-2/3 md:w-3/4">
-                <MessageContainer />
-            </div>
-        </div>
-    );
+	return (
+		<div className='flex flex-col sm:flex-row h-[100vh] sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+			<div className="w-full sm:w-auto h-1/3 sm:h-auto">
+				<Sidebar />
+			</div>
+			<div className="w-full h-2/3 sm:h-auto sm:flex-grow">
+				<MessageContainer />
+			</div>
+		</div>
+	);
 };
 export default Home;
